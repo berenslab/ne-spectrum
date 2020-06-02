be8f5546061e2b50509d72ac0e91c6ac2191ab9b
Niklas Böhm <jan-niklas.boehm@student.uni-tuebingen.de>
2020-06-02 16:01:53 +0200

    Make treutlein-preprocess.py runnable
    Instead of just having the exported jupyter notebook, this is a tiny
    command line application that will do the preprocessing pipeline

---
+++ static/rnaseqTools.py
@@ -0,0 +1,353 @@
+import numpy as np
+import pylab as plt
+import pandas as pd
+from scipy import sparse
+
+
+def sparseload(
+    filename, sep=",", dtype=float, chunksize=1000, index_col=0, droplastcolumns=0
+):
+    with open(filename) as file:
+        genes = []
+        sparseblocks = []
+        for i, chunk in enumerate(
+            pd.read_csv(filename, chunksize=chunksize, sep=sep, index_col=index_col)
+        ):
+            print(".", end="", flush=True)
+            if i == 0:
+                cells = np.array(chunk.columns)
+            genes.extend(list(chunk.index))
+            sparseblock = sparse.csr_matrix(chunk.values.astype(dtype))
+            sparseblocks.append([sparseblock])
+        counts = sparse.bmat(sparseblocks)
+        print(" done")
+
+    if droplastcolumns > 0:
+        end = cells.size - droplastcolumns
+        cells = cells[:end]
+        counts = counts[:, :end]
+
+    return (counts.T, np.array(genes), cells)
+
+
+def geneSelection(
+    data,
+    threshold=0,
+    atleast=10,
+    yoffset=0.02,
+    xoffset=5,
+    decay=1.5,
+    n=None,
+    plot=True,
+    markers=None,
+    genes=None,
+    figsize=(6, 3.5),
+    markeroffsets=None,
+    labelsize=10,
+    alpha=1,
+    verbose=1,
+):
+
+    if sparse.issparse(data):
+        zeroRate = 1 - np.squeeze(np.array((data > threshold).mean(axis=0)))
+        A = data.multiply(data > threshold)
+        A.data = np.log2(A.data)
+        meanExpr = np.zeros_like(zeroRate) * np.nan
+        detected = zeroRate < 1
+        meanExpr[detected] = np.squeeze(np.array(A[:, detected].mean(axis=0))) / (
+            1 - zeroRate[detected]
+        )
+    else:
+        zeroRate = 1 - np.mean(data > threshold, axis=0)
+        meanExpr = np.zeros_like(zeroRate) * np.nan
+        detected = zeroRate < 1
+        mask = data[:, detected] > threshold
+        logs = np.zeros_like(data[:, detected]) * np.nan
+        logs[mask] = np.log2(data[:, detected][mask])
+        meanExpr[detected] = np.nanmean(logs, axis=0)
+
+    lowDetection = np.array(np.sum(data > threshold, axis=0)).squeeze() < atleast
+    zeroRate[lowDetection] = np.nan
+    meanExpr[lowDetection] = np.nan
+
+    if n is not None:
+        up = 10
+        low = 0
+        for t in range(100):
+            nonan = ~np.isnan(zeroRate)
+            selected = np.zeros_like(zeroRate).astype(bool)
+            selected[nonan] = (
+                zeroRate[nonan] > np.exp(-decay * (meanExpr[nonan] - xoffset)) + yoffset
+            )
+            if np.sum(selected) == n:
+                break
+            elif np.sum(selected) < n:
+                up = xoffset
+                xoffset = (xoffset + low) / 2
+            else:
+                low = xoffset
+                xoffset = (xoffset + up) / 2
+        if verbose > 0:
+            print("Chosen offset: {:.2f}".format(xoffset))
+    else:
+        nonan = ~np.isnan(zeroRate)
+        selected = np.zeros_like(zeroRate).astype(bool)
+        selected[nonan] = (
+            zeroRate[nonan] > np.exp(-decay * (meanExpr[nonan] - xoffset)) + yoffset
+        )
+
+    if plot:
+        import seaborn as sns
+
+        if figsize is not None:
+            plt.figure(figsize=figsize)
+        plt.ylim([0, 1])
+        if threshold > 0:
+            plt.xlim([np.log2(threshold), np.ceil(np.nanmax(meanExpr))])
+        else:
+            plt.xlim([0, np.ceil(np.nanmax(meanExpr))])
+        x = np.arange(plt.xlim()[0], plt.xlim()[1] + 0.1, 0.1)
+        y = np.exp(-decay * (x - xoffset)) + yoffset
+        if decay == 1:
+            plt.text(
+                0.4,
+                0.2,
+                "{} genes selected\ny = exp(-x+{:.2f})+{:.2f}".format(
+                    np.sum(selected), xoffset, yoffset
+                ),
+                color="k",
+                fontsize=labelsize,
+                transform=plt.gca().transAxes,
+            )
+        else:
+            plt.text(
+                0.4,
+                0.2,
+                "{} genes selected\ny = exp(-{:.1f}*(x-{:.2f}))+{:.2f}".format(
+                    np.sum(selected), decay, xoffset, yoffset
+                ),
+                color="k",
+                fontsize=labelsize,
+                transform=plt.gca().transAxes,
+            )
+
+        plt.plot(x, y, color=sns.color_palette()[1], linewidth=2)
+        xy = np.concatenate(
+            (
+                np.concatenate((x[:, None], y[:, None]), axis=1),
+                np.array([[plt.xlim()[1], 1]]),
+            )
+        )
+        t = plt.matplotlib.patches.Polygon(xy, color=sns.color_palette()[1], alpha=0.4)
+        plt.gca().add_patch(t)
+
+        plt.scatter(meanExpr, zeroRate, s=1, alpha=alpha, rasterized=True)
+        if threshold == 0:
+            plt.xlabel("Mean log2 nonzero expression")
+            plt.ylabel("Frequency of zero expression")
+        else:
+            plt.xlabel("Mean log2 nonzero expression")
+            plt.ylabel("Frequency of near-zero expression")
+        plt.tight_layout()
+
+        if markers is not None and genes is not None:
+            if markeroffsets is None:
+                markeroffsets = [(0, 0) for g in markers]
+            for num, g in enumerate(markers):
+                i = np.where(genes == g)[0]
+                plt.scatter(meanExpr[i], zeroRate[i], s=10, color="k")
+                dx, dy = markeroffsets[num]
+                plt.text(
+                    meanExpr[i] + dx + 0.1,
+                    zeroRate[i] + dy,
+                    g,
+                    color="k",
+                    fontsize=labelsize,
+                )
+
+    return selected
+
+
+# Computing the matrix of Euclidean distances
+def pdist2(A, B):
+    D = (
+        np.sum(A ** 2, axis=1, keepdims=True)
+        + np.sum(B ** 2, axis=1, keepdims=True).T
+        - 2 * A @ B.T
+    )
+    return D
+
+
+import warnings
+
+# Computing the matrix of correlations
+def corr2(A, B):
+    A = A - A.mean(axis=1, keepdims=True)
+    B = B - B.mean(axis=1, keepdims=True)
+    ssA = (A ** 2).sum(axis=1, keepdims=True)
+    ssB = (B ** 2).sum(axis=1, keepdims=True)
+    # this ignores the NaN warnings. The result can have nans!
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        C = np.dot(A, B.T) / np.sqrt(np.dot(ssA, ssB.T))
+    return C
+
+
+def map_to_tsne(
+    referenceCounts,
+    referenceGenes,
+    newCounts,
+    newGenes,
+    referenceAtlas,
+    bootstrap=False,
+    knn=10,
+    nrep=100,
+    seed=None,
+    batchsize=1000,
+    verbose=1,
+):
+    gg = sorted(list(set(referenceGenes) & set(newGenes)))
+    if verbose > 0:
+        print("Using a common set of " + str(len(gg)) + " genes.")
+
+    newGenes = [np.where(newGenes == g)[0][0] for g in gg]
+    refGenes = [np.where(referenceGenes == g)[0][0] for g in gg]
+    X = newCounts[:, newGenes]
+    if sparse.issparse(X):
+        X = np.array(X.todense())
+    X = np.log2(X + 1)
+    T = referenceCounts[:, refGenes]
+    if sparse.issparse(T):
+        T = np.array(T.todense())
+    T = np.log2(T + 1)
+
+    n = X.shape[0]
+    assignmentPositions = np.zeros((n, referenceAtlas.shape[1]))
+    batchCount = int(np.ceil(n / batchsize))
+    if (batchCount > 1) and (verbose > 0):
+        print("Processing in batches", end="", flush=True)
+    for b in range(batchCount):
+        if (batchCount > 1) and (verbose > 0):
+            print(".", end="", flush=True)
+        batch = np.arange(b * batchsize, np.minimum((b + 1) * batchsize, n))
+        C = corr2(X[batch, :], T)
+        ind = np.argpartition(C, -knn)[:, -knn:]
+        for i in range(batch.size):
+            assignmentPositions[batch[i], :] = np.median(
+                referenceAtlas[ind[i, :], :], axis=0
+            )
+    if (batchCount > 1) and (verbose > 0):
+        print(" done", flush=True)
+
+    # Note: currently bootstrapping does not support batchsize
+    if bootstrap:
+        if seed is not None:
+            np.random.seed(seed)
+        assignmentPositions_boot = np.zeros((n, referenceAtlas.shape[1], nrep))
+        if verbose > 0:
+            print("Bootstrapping", end="", flush=True)
+        for rep in range(nrep):
+            if verbose > 0:
+                print(".", end="")
+            bootgenes = np.random.choice(T.shape[1], T.shape[1], replace=True)
+            C_boot = corr2(X[:, bootgenes], T[:, bootgenes])
+            ind = np.argpartition(C_boot, -knn)[:, -knn:]
+            for i in range(X.shape[0]):
+                assignmentPositions_boot[i, :, rep] = np.median(
+                    referenceAtlas[ind[i, :], :], axis=0
+                )
+        if verbose > 0:
+            print(" done")
+        return (assignmentPositions, assignmentPositions_boot)
+    else:
+        return assignmentPositions
+
+
+def map_to_clusters(
+    referenceCounts,
+    referenceGenes,
+    newCounts,
+    newGenes,
+    referenceClusters,
+    referenceClusterNames=[],
+    cellNames=[],
+    bootstrap=False,
+    nrep=100,
+    seed=None,
+    verbose=False,
+    until=0.95,
+    returnCmeans=False,
+    totalClusters=None,
+):
+
+    gg = sorted(list(set(referenceGenes) & set(newGenes)))
+    print("Using a common set of " + str(len(gg)) + " genes.")
+
+    newGenes = [np.where(newGenes == g)[0][0] for g in gg]
+    refGenes = [np.where(referenceGenes == g)[0][0] for g in gg]
+    X = newCounts[:, newGenes]
+    if sparse.issparse(X):
+        X = np.array(X.todense())
+    X = np.log2(X + 1)
+    T = referenceCounts[:, refGenes]
+    if sparse.issparse(T):
+        T = np.array(T.todense())
+    T = np.log2(T + 1)
+
+    if totalClusters is not None:
+        K = totalClusters
+    else:
+        K = np.max(referenceClusters) + 1
+    means = np.zeros((K, T.shape[1]))
+    for c in range(K):
+        if np.sum(referenceClusters == c) > 0:
+            means[c, :] = np.mean(T[referenceClusters == c, :], axis=0)
+
+    Cmeans = corr2(X, means)
+    allnans = np.sum(np.isnan(Cmeans), axis=1) == Cmeans.shape[1]
+    clusterAssignment = np.zeros(Cmeans.shape[0]) * np.nan
+    clusterAssignment[~allnans] = np.nanargmax(Cmeans[~allnans, :], axis=1)
+
+    if bootstrap:
+        if seed is not None:
+            np.random.seed(seed)
+
+        clusterAssignment_boot = np.zeros((X.shape[0], nrep), dtype=int)
+        for rep in range(nrep):
+            print(".", end="", flush=True)
+            bootgenes = np.random.choice(T.shape[1], T.shape[1], replace=True)
+            Cmeans_boot = corr2(X[:, bootgenes], means[:, bootgenes])
+            m = np.zeros(Cmeans.shape[0]) * np.nan
+            m[~allnans] = np.nanargmax(Cmeans_boot[~allnans, :], axis=1)
+            clusterAssignment_boot[:, rep] = m
+        print(" done")
+
+        clusterAssignment_matrix = np.zeros((X.shape[0], K))
+        for cell in range(X.shape[0]):
+            mapsto, mapsto_counts = np.unique(
+                clusterAssignment_boot[cell, :], return_counts=True
+            )
+            for i, m in enumerate(mapsto):
+                clusterAssignment_matrix[cell, m] = mapsto_counts[i] / nrep
+
+        if verbose:
+            for rownum, row in enumerate(clusterAssignment_matrix):
+                ind = np.argsort(row)[::-1]
+                ind = ind[: np.where(np.cumsum(row[ind]) >= until)[0][0] + 1]
+                mystring = []
+                for i in ind:
+                    s = referenceClusterNames[i] + " ({:.1f}%)".format(100 * row[i])
+                    mystring.append(s)
+                mystring = cellNames[rownum] + ": " + ", ".join(mystring)
+                print(mystring)
+
+        if returnCmeans:
+            return clusterAssignment, clusterAssignment_matrix, Cmeans
+        else:
+            return clusterAssignment, clusterAssignment_matrix
+
+    else:
+        if returnCmeans:
+            return clusterAssignment, Cmeans
+        else:
+            return clusterAssignment
--- static/treutlein-preprocess.py
+++ static/treutlein-preprocess.py
@@ -3,140 +3,112 @@
 
 # # Treutlein lab ape organoid data
 # The data are here: https://www.ebi.ac.uk/arrayexpress/experiments/E-MTAB-7552/
-# 
-# Download files 1 to 7  
-# https://www.ebi.ac.uk/arrayexpress/files/E-MTAB-7552/E-MTAB-7552.processed.1.zip  
-# https://www.ebi.ac.uk/arrayexpress/files/E-MTAB-7552/E-MTAB-7552.processed.2.zip  
-# https://www.ebi.ac.uk/arrayexpress/files/E-MTAB-7552/E-MTAB-7552.processed.3.zip  
-# https://www.ebi.ac.uk/arrayexpress/files/E-MTAB-7552/E-MTAB-7552.processed.4.zip  
-# https://www.ebi.ac.uk/arrayexpress/files/E-MTAB-7552/E-MTAB-7552.processed.5.zip  
-# https://www.ebi.ac.uk/arrayexpress/files/E-MTAB-7552/E-MTAB-7552.processed.6.zip  
-# https://www.ebi.ac.uk/arrayexpress/files/E-MTAB-7552/E-MTAB-7552.processed.7.zip  
+#
+# Download files 1 to 7
+# https://www.ebi.ac.uk/arrayexpress/files/E-MTAB-7552/E-MTAB-7552.processed.1.zip
+# https://www.ebi.ac.uk/arrayexpress/files/E-MTAB-7552/E-MTAB-7552.processed.2.zip
+# https://www.ebi.ac.uk/arrayexpress/files/E-MTAB-7552/E-MTAB-7552.processed.3.zip
+# https://www.ebi.ac.uk/arrayexpress/files/E-MTAB-7552/E-MTAB-7552.processed.4.zip
+# https://www.ebi.ac.uk/arrayexpress/files/E-MTAB-7552/E-MTAB-7552.processed.5.zip
+# https://www.ebi.ac.uk/arrayexpress/files/E-MTAB-7552/E-MTAB-7552.processed.6.zip
+# https://www.ebi.ac.uk/arrayexpress/files/E-MTAB-7552/E-MTAB-7552.processed.7.zip
 # and unpack
-# 
-# Download supplementary informationfrom the Nature paper https://static-content.springer.com/esm/art%3A10.1038%2Fs41586-019-1654-9/MediaObjects/41586_2019_1654_MOESM3_ESM.zip, extract `Supplementary_Table_5.txt` and rename it into `metadata_macaque_cells_suppl.tsv`. The metadata file on arrayexpress seems to be wrong. I wrote to the authors to clarify.
-
-# In[3]:
-
-
-# Prepare
-
-get_ipython().run_line_magic('matplotlib', 'notebook')
+#
+# Download supplementary informationfrom the Nature paper https://static-content.springer.com/esm/art%3A10.1038%2Fs41586-019-1654-9/MediaObjects/41586_2019_1654_MOESM3_ESM.zip, extract `Supplementary_Table_5.txt` and rename it into `metadata_chimp_cells_suppl.tsv`. The metadata file on arrayexpress seems to be wrong. I wrote to the authors to clarify.
 
 import numpy as np
-import pylab as plt
-import seaborn as sns
 import pandas as pd
 import matplotlib
-import pickle
 import scipy
-from scipy import sparse
-
-sns.set_style('ticks')
-
 import openTSNE
 
-# get rnaseqTool.py from berenslab/rna-seq-tsne
+from scipy import sparse
+from scipy.io import mmread
+from pathlib import Path
+
 import rnaseqTools
 
 
-# In[37]:
+def preprocess(metafile, countfile, line, n=1000, decay=1.5, n_components=50):
+    meta = pd.read_csv(metafile, sep="\t")
+
+    counts = mmread(str(countfile))
+    counts = scipy.sparse.csc_matrix(counts).T
+
+    ind = meta["in_FullLineage"].values
+    if line is not None:
+        ind = ind & (meta["Line"].values == line)
+
+    seqDepths = np.array(counts[ind, :].sum(axis=1))
+    stage = meta["Stage"].values[ind].astype("str")
+
+    impGenes = rnaseqTools.geneSelection(counts[ind, :], n=n, decay=decay, plot=False)
+
+    # Transformations
+
+    X = np.log2(counts[:, impGenes][ind, :] / seqDepths * np.median(seqDepths) + 1)
+    X = np.array(X)
+    X = X - X.mean(axis=0)
+    U, s, V = np.linalg.svd(X, full_matrices=False)
+    X = np.dot(U, np.diag(s))
+    X = X[:, np.argsort(s)[::-1]][:, :n_components]
+
+    return X, stage
 
 
-# Uncomment what you want
+if __name__ == "__main__":
+    import argparse
 
-metafile  = "metadata_chimp_cells_suppl.tsv"
-countfile = "chimp_cell_counts_consensus.mtx"
-line = None
-outputfile = "chimp.pickle"
+    parser = argparse.ArgumentParser(
+        "Preprocess developmental data.  This requires the data to be "
+        "downloaded and unzipped already, prior to running this.  Read "
+        "the comments at the top of this file in order to get the links "
+        " that point to the data files and some further instructions."
+    )
+    parser.add_argument(
+        "--type",
+        default="chimp",
+        help="Whether chimp or human data should be extracted",
+    )
+    parser.add_argument("--line", default=None, help="Which line should be saved")
+    parser.add_argument(
+        "--prefix",
+        type=Path,
+        default="~/Downloads/treutlein",
+        help="The location of the raw data",
+    )
+    parser.add_argument(
+        "--outfmt", default="npy", help="Whether to output npy or pickle files"
+    )
 
-# metafile  = "metadata_human_cells.tsv"
-# countfile = "human_cell_counts_consensus.mtx"
-# line = '409b2'
-# outputfile = "human-409b2.pickle"
+    args = parser.parse_args()
 
-# metafile  = "metadata_human_cells.tsv"
-# countfile = "human_cell_counts_consensus.mtx"
-# line = 'H9'
-# outputfile = "human-h9.pickle"
+    # select a default line for human
+    if args.type == "human" and (args.line is None):
+        args.line = "409b2"
 
+    if args.type == "chimp":
+        metafile = "metadata_chimp_cells_suppl.tsv"
+        countfile = "chimp_cell_counts_consensus.mtx"
+        line = None
+    elif args.type == "human":
+        metafile = "metadata_human_cells.tsv"
+        countfile = "human_cell_counts_consensus.mtx"
+        line = args.line
 
-# In[39]:
+    if line not in [None, "409b2", "H9"]:
+        raise ValueError(f"Value for line {line} is not valid.")
 
+    metafile = args.prefix.expanduser() / metafile
+    countfile = args.prefix.expanduser() / countfile
+    X, stage = preprocess(metafile, countfile, line)
 
-meta = pd.read_csv('../../../../Downloads/' + metafile, sep='\t')
+    outputfile = "{}{}".format(args.type, "" if args.line is None else f"-{args.line}")
 
-print(meta['in_FullLineage'].sum())
-print(meta['Stage'].unique())
-print(np.unique(meta['Line'].values[meta['in_FullLineage'].values]))
+    if args.outfmt == "pickle":
+        import pickle
 
-
-# In[40]:
-
-
-# genes = pd.read_csv('../../../../Downloads/genes_consensus.txt', sep='\t', names=['id','name'])
-# genes = genes['name'].values.squeeze().astype(str)
-# print(genes)
-
-
-# In[7]:
-
-
-from scipy.io import mmread
-
-get_ipython().run_line_magic('time', "counts = mmread('../../../../Downloads/' + countfile)")
-counts = scipy.sparse.csc_matrix(counts).T
-
-
-# In[8]:
-
-
-ind = meta['in_FullLineage'].values
-if line is not None:
-    ind = ind & (meta['Line'].values == line)
-
-seqDepths = np.array(counts[ind,:].sum(axis=1))
-stage = meta['Stage'].values[ind].astype('str')
-
-impGenes  = rnaseqTools.geneSelection(counts[ind,:], n=1000, decay=1.5, plot=True)
-
-
-# In[9]:
-
-
-# Transformations
-
-X = np.log2(counts[:, impGenes][ind,:] / seqDepths * np.median(seqDepths) + 1)  
-X = np.array(X)
-X = X - X.mean(axis=0)
-get_ipython().run_line_magic('time', 'U,s,V = np.linalg.svd(X, full_matrices=False)')
-X = np.dot(U, np.diag(s))
-X = X[:, np.argsort(s)[::-1]][:,:50]
-
-print(X.shape)
-
-
-# In[14]:
-
-
-pickle.dump([X, stage], open(outputfile, 'wb'))
-
-
-# In[33]:
-
-
-# %time Z = fast_tsne(X)   
-
-
-# In[32]:
-
-
-# plt.figure(figsize=(5,5))
-# for stage in np.unique(meta['Stage'].values[ind]):
-#     subset = meta['Stage'].values[ind] == stage
-#     plt.scatter(Z[subset,0], Z[subset,1], s=1, label=stage)
-
-# plt.legend()
-# sns.despine()
-# plt.tight_layout()
-
+        pickle.dump([X, stage], open(outputfile + ".pickle", "wb"))
+    elif args.outfmt == "npy":
+        np.save(outputfile + ".data.npy", X)
+        np.save(outputfile + ".labels.npy", stage)

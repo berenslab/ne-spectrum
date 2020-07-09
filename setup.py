df1515e0314cc5b3c41c815e4a9b4707e2adb8fe
Niklas Böhm <jan-niklas.boehm@student.uni-tuebingen.de>
2020-07-09 13:49:24 +0200

    Add cython extensions to setup.py
    Can be built with `python setup.py --build_ext`.

--- setup.py
+++ setup.py
@@ -1,4 +1,24 @@
 from setuptools import setup
+from distutils.extension import Extension
+from Cython.Build import cythonize
+
+import numpy
+
+bh_umap = Extension(
+    name="bhumap",
+    sources=["jnb_msc/transformer/bhumap.pyx"],
+    library_dirs=["jnb_msc"],
+    ## This needs to be adapted for the respective location
+    include_dirs=["../../../msc/vendor/python/openTSNE/", numpy.get_include()],
+)
+
+bh_noack = Extension(
+    name="bhnoack",
+    sources=["jnb_msc/transformer/bhnoack.pyx"],
+    library_dirs=["jnb_msc"],
+    ## This needs to be adapted for the respective location
+    include_dirs=["../../../msc/vendor/python/openTSNE/", numpy.get_include()],
+)
 
 setup(
     name="jnb_msc",
@@ -8,4 +28,5 @@ setup(
     author="Niklas Böhm",
     author_email="jan-niklas.boehm@student.uni-tuebingen.de",
     packages=["jnb_msc"],
+    ext_modules=cythonize([bh_umap, bh_noack]),
 )

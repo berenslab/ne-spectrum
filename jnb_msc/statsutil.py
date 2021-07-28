b6dbd5747435191dc72dc2003546d3702b7591ce
Niklas Böhm <jan-niklas.boehm@student.uni-tuebingen.de>
2021-07-28 14:43:58 +0200

    Update statsutil to be more flexible
    Can now compute the distance correlations of more layouts and
    do that in parallel.

--- jnb_msc/statsutil.py
+++ jnb_msc/statsutil.py
@@ -3,12 +3,12 @@ from . import redo
 import sys
 import os
 import inspect
+from pathlib import Path
+from multiprocessing.pool import Pool
 
 import dcor
 import numpy as np
-
-from pathlib import Path
-
+from umap.umap_ import find_ab_params
 
 def correlate(x, y):
     return np.sqrt(dcor.u_distance_correlation_sqr(x, y))
@@ -25,31 +25,47 @@ def tsne_from_rho(rho, dsrc):
     return dsrc / tsne / "data.npy"
 
 
+def do_tsne(arg):
+    ref = arg["ref"]
+    tsnes = arg["tsnes"]
+    subsel = arg["subsel"]
+
+    return [correlate(ref[subsel], np.load(tsne)[subsel]) for tsne in tsnes]
+
 def correlate_dataset(dsrc, rhos, n_subsel=5000, random_state=None):
     if random_state is None:
         random_state = np.random.RandomState(555)
-    fa2 = dsrc / "ann/stdscale;f:1e3/fa2/data.npy"
-    umap = dsrc / "umap_knn/maxscale;f:10/umap/data.npy"
+
+    min_dist = 0.1
+    spread = 1.0
+    a, b = find_ab_params(spread, min_dist)
+
+    # order here is important and needs to be accounted for when using the dict
+    setup = {
+        "fa2": dsrc / "ann/stdscale;f:1e3/fa2/data.npy",
+        "umap": dsrc / "umap_knn/maxscale;f:10/umap/data.npy",
+        "fa2-ri": dsrc / "ann/random/stdscale;f:1e3/fa2/data.npy",
+        "umap-default": dsrc / f"umap_knn/spectral/maxscale;f:10/umap;a:{a};b:{b}/data.npy",
+    }
 
     tsnes = [tsne_from_rho(rho, dsrc) for rho in rhos]
-    datafiles = [fa2, umap] + tsnes
+    datafiles = list(setup.values()) + tsnes
 
     # the computation happens here
     redo.redo_ifchange(datafiles)
 
-    fa2, umap = np.load(fa2), np.load(umap)
 
+    n = np.load(dsrc / "data.npy").shape[0]
     subsel = random_state.choice(
-        fa2.shape[0], min(n_subsel, fa2.shape[0]), replace=False
+        n, min(n_subsel, n), replace=False
     )
-    corr_fa2 = []
-    corr_umap = []
-    for tsne_f in tsnes:
-        tsne = np.load(tsne_f)
-        corr_fa2.append(correlate(fa2[subsel], tsne[subsel]))
-        corr_umap.append(correlate(umap[subsel], tsne[subsel]))
+    corrs = {k: [] for k in setup.keys()}
 
-    return corr_fa2, corr_umap
+    algo_data = [np.load(f) for f in setup.values()]
+    args = [dict(ref=a, tsnes=tsnes, subsel=subsel) for a in algo_data]
+    results = Pool(len(setup)).map(do_tsne, args)
+
+    return {k: res for k, res in zip(setup.keys(), results)}
 
 
 def get_rhos():
